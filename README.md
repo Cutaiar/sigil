f3810790bcdc062bb74c6a5cc5e97a6142f58aa2
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-04-08 23:53:50 -0600

    Add vite boiler plate

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sigil
+
+Execute arbitrary call data on the blockchain
+
+## Client
+
+```bash
+cd client
+bun i
+bun run dev
+```
---
+++ client/eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  extends: [
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+  ],
+  languageOptions: {
+    parserOptions: {
+      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  plugins: {
+    "react-x": reactX,
+    "react-dom": reactDom,
+  },
+  rules: {
+    ...reactX.configs["recommended-typescript"].rules,
+    ...reactDom.configs.recommended.rules,
+  },
+});
